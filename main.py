ecb46dc2f1b642de411cf46c5558720a2307a542
mr-haseeb <bolahaseeb@gmail.com>
2021-04-12 11:20:08 +0500

    route and query parameter

---
+++ main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI 
+from typing import Optional
+from pydantic import BaseModel
+
+app=FastAPI()
+
+@app.get('/blog')
+def index(limit=10,published=True,sort: Optional[str]=None):
+    # only get 10 published blogs
+    if published:
+        return {'data':f'{limit} published blog from db'}
+    
+    else:
+        return {'data':f'{limit} blog from db'}
+
+
+@app.get('/blog/unpublised')
+def unpublised():
+    return 'unpublished'
+
+
+
+@app.get('/blog/{id}')
+def show(id:int):
+    #fetch blog with id=id
+    return {'data':id}
+
+
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id,limit=10):
+    #fetch comments of blog with id=id
+    return {'data':{'1','2'}}
+
+
+
+# for request  body
+class Blog(BaseModel):
+    title:str
+    body:str
+    published_at:Optional[bool]
+
+
+
+
+@app.post('/blog/create ')
+def create_blog(blog:Blog):
+    return {'data':f"Blog is created with title as {blog.title}"}
+
+    
